c63e9c6c9722c7125c4a6f01d765d46296f505d2
Siddeshwar Gauncar <sddshwr@gmail.com>
2018-07-04 18:49:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,10 @@
 var name = "Susan"
 var height = 74
 var message = ` ${name} is ${height} inches tall`
-
+describe('Height', () => {
+-    it('is greater than 40', () => {
++    it('is less than 40', () => {
+       expect(index.height).toBeLessThan(40)
+     })
+   })
 module.exports = {name, height, message}
