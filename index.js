67039820a779dc56dbaf978bf809fda31f5ae2e9
Siddeshwar Gauncar <sddshwr@gmail.com>
2018-07-04 18:43:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
-var name = "Joe"
+var name = "Susan"
 var height = 74
-var message = ` is ${height} inches tall`
-
+var message = ` ${name} is ${height} inches tall`
+if (height < 40){console.log("Expected  height to be less than 40")} 
 module.exports = {name, height, message}
